c928a4b2fec5c07862c8aa64fcc89c9294c16c4c
Abhishek pandey <abhie9336@gmail.com>
2022-10-17 22:34:41 +0530

    added dark mode

--- style/index.js
+++ style/index.js
@@ -1,23 +1,5 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./components/App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
-
-//CHALLENGE: Make the code in App.jsx work.
-//The final app should have a single contact
-//with fName, lName and email.
-
-//HINT: You'll need to apply the following things you learnt:
-//1. Using JS Objects with state.
-//2. Making use of previous state when changing state.
-//3. Working with forms in React.
-//4. Handing events
 function PageTransitions(){
-const themeBtn = document.querySelector('.theme-btn');
-themeBtn.addEventListener('click', ()=>{
-    let element = document.body;
-    element.classList.toggle('light-mode');
-})
+var themeBtn = document.getElementById("main-body");
+themeBtn.classList.toggle("light-mode");
 }
-PageTransitions();
